2f17eb18efec5d3a8ffd4f5be2db6d120e05275e
JaoMiranda <joao.araujo.estagiario@icmbio.gov.br>
2023-03-17 10:54:56 -0300

    django

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,9 @@
-#from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('HOME 1')
+    return render(request, 'home.html')
 
 def contato(request):
     return HttpResponse('contato')
